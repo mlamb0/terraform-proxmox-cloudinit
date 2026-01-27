2cb0b7bf4de3e85b39825722b988d6cbd21473b2
Mitchell <mitchell_lamb@mail.com>
2026-01-27 18:59:04 -0500

    removed comment

--- versions.tf
+++ versions.tf
@@ -6,15 +6,3 @@ terraform {
     }
   }
 }
-
-/*
-
-This is needed in the consuming providers.tf file:
-
-provider "proxmox" {
-  pm_api_url = " "
-  pm_api_token_id = " "
-  pm_api_token_secret = " "
-}
-
-*/
